5ab5bb2586277103b0879291f7a7122cfe1a50b3
Mikołaj <mikolajc98@gmail.com>
2022-01-07 00:18:15 +0100

    Added unit test project to solution. Merge tests included.

---
+++ EffectTests/EffectMergeTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EffectCore;
+using System.Collections.Generic;
+
+namespace EffectTests
+{
+    [TestClass]
+    public class EffectMergeTest
+    {
+        [TestMethod]
+        public void MergeSuccessTest()
+        {
+            Effect expected = Effect.Successful();
+
+            IEnumerable<Effect> effects = new List<Effect>()
+            {
+                Effect.Successful(),
+                Effect.Successful("OK"),                
+                Effect.Successful("Done")
+            };
+
+            Effect actual = Effect.Merge(effects);
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void MergeFailureTest()
+        {
+            Effect expected = Effect.Failure("Error 1.\r\nError 2.\r\nError 3.");
+
+            IEnumerable<Effect> effects = new List<Effect>()
+            {
+                Effect.Successful(),
+                Effect.Successful("Test"),
+                Effect.Failure("Error 1."),
+                Effect.Successful(),
+                Effect.Failure("Error 2."),
+                Effect.Failure("Error 3.")
+            };
+
+            Effect actual = Effect.Merge(effects);
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
---
+++ EffectTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EffectTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EffectTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6b3821bc-6d03-449e-9084-a615804ac4c5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
